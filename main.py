e5bc55c7e2b4aa2c4c03cee15535e3760b0391de
Bakyt Zhansaya <zansaabakyt364@gmail.com>
2023-03-02 14:51:17 +0600

    ffiiiiisst commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#a, b, c нақты сандары берілген. a < b < c теңсіздіктердің орындалу - орындалмауын
+#тексеру
+print( "   29    ----- ")
+p = float(input("p = "))
+o = float(input("o = "))
+i = float(input("i = "))
+if(p<o<i):
+    print("   Tensizdic oryndalady  ")
+
+else:
+    print(" Tensizdic oryndalmaiddu")
